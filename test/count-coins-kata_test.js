8362daf1853905d79fe05c92a53694a03d6d0756
Per Flitig <per.flitig@gmail.com>
2014-03-15 15:08:18 +0100

    Added the first few tests, not all of them passing

--- test/count-coins-kata_test.js
+++ test/count-coins-kata_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var count_coins_kata = require('../lib/count-coins-kata.js');
+var coins = require('../lib/count-coins-kata.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +22,55 @@ var count_coins_kata = require('../lib/count-coins-kata.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
+var testCount = function(test, x, expected) {
+  test.expect(1);
+  test.deepEqual(coins.count(x), expected);
+  test.done();
+};
+
+exports.coins = {
+  '0 cents makes 0 change': function(test) {
+    testCount(test, 0, [[0,0,0,0]]);
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(count_coins_kata.awesome(), 'awesome', 'should be awesome.');
-    test.done();
+  '1 cent makes 1 change': function(test) {
+    testCount(test, 1, [[0,0,0,1]]);
+  },
+  '3 cent makes 1 change': function(test) {
+    testCount(test, 3, [[0,0,0,3]]);
+  },
+  '5 cents makes 2 change': function(test) {
+    testCount(test, 5, [[0,0,1,0],[0,0,0,5]]);
+  },
+  '8 cents makes 2 change': function(test) {
+    testCount(test, 8, [[0,0,1,0],[0,0,0,8]]);
+  },
+  '10 cents makes 3 change': function(test) {
+    testCount(test, 10, [[0,1,0,0],[0,0,2,0],[0,0,0,10]]);
+  },
+  '12 cents makes 3 change': function(test) {
+    testCount(test, 12, [[0,1,0,2],[0,0,2,2],[0,0,0,12]]);
+  },
+  '15 cents makes 6 change': function(test) {
+    testCount(test, 15, [
+      [0,1,1,0],
+      [0,1,0,5],
+      [0,0,3,0],
+      [0,0,2,5],
+      [0,0,1,10],
+      [0,0,0,15]
+    ]);
+  },
+  '20 cents makes 3 change': function(test) {
+    testCount(test, 20, [
+      [0,2,0,0],
+      [0,1,2,0],
+      [0,1,1,5],
+      [0,1,0,10],
+      [0,0,4,0],
+      [0,0,3,5],
+      [0,0,2,10],
+      [0,0,1,15],
+      [0,0,0,20]
+    ]);
   },
 };
